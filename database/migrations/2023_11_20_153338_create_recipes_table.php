b265cbb19c7e458aa092b627c1a592101b93299a
Renaud Bresson <renaud.cci@gmail.com>
2023-11-20 16:44:17 +0100

    create/model-Recipe

---
+++ database/migrations/2023_11_20_153338_create_recipes_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->json('ingredients');
+            $table->string('preparationTime');
+            $table->string('cookingTime');
+            $table->integer('serves');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
